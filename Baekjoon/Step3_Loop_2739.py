874a95068aaeaf1375736ef9344340626308b3b0
haenginthere321 <ratms7239@gmail.com>
2026-04-28 14:58:31 +1000

    Update Step3_Loop_2739.py

--- Baekjoon/Step3_Loop_2739.py
+++ Baekjoon/Step3_Loop_2739.py
@@ -1,3 +1,9 @@
-a = int(input())
+"""
+Problem: [Baekjoon 2739] multiplication table
+Concept: Loop (for statement, format)
+Description: Given an integer N (1<=N<=9), print multiplication table for N.
+"""
+
+N = int(input())
 for i in range(1,10):
-  print("{} * {} = {}".format(a,i,a*i))
+  print("{} * {} = {}".format(N,i,N*i))
