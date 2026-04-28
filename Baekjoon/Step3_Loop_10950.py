2379e8c31ed4f29da5b0e46c4d35b0f3a56284e3
haenginthere321 <ratms7239@gmail.com>
2026-04-28 15:04:23 +1000

    Update Step3_Loop_10950.py

--- Baekjoon/Step3_Loop_10950.py
+++ Baekjoon/Step3_Loop_10950.py
@@ -1,3 +1,9 @@
+"""
+Problem: [Baekjoon 10950] A+B - 3
+Concept: Loop (for statement, list)
+Description: Given the number of test cases, take two integers A and B for each, and print A+B.
+"""
+
 a = int(input())
 b = [list(map(int,input().split())) for i in range(a)]
 
