98a4ee48be87dd4e87f270eb1523ba403d040a92
haenginthere321 <ratms7239@gmail.com>
2026-04-28 15:38:38 +1000

    Create Step6_Advanced1_25206.py

---
+++ Baekjoon/Step6_Advanced1_25206.py
@@ -0,0 +1,22 @@
+grade = {
+    'A+': 4.5, 'A0': 4.0,
+    'B+': 3.5, 'B0': 3.0,
+    'C+': 2.5, 'C0': 2.0,
+    'D+': 1.5, 'D0': 1.0,
+    'F': 0.0
+}
+
+total_score = 0
+total_credit = 0
+
+for i in range(20):
+    name, credit, score = input().split()
+    credit = float(credit)
+
+    if score == 'P':
+        continue
+
+    total_score += credit * grade[score]
+    total_credit += credit
+
+print(total_score / total_credit)
