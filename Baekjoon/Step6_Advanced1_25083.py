5e859768423a75dac5a1caf9d06f82217c3216f7
haenginthere321 <ratms7239@gmail.com>
2026-04-28 15:35:34 +1000

    Create Step6_Advanced1_25083.py

---
+++ Baekjoon/Step6_Advanced1_25083.py
@@ -0,0 +1,17 @@
+# Solution 1
+
+print(r"""         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |""")
+
+# Solution 2
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
